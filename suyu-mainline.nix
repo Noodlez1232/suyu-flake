b3904f149f124bc485e1cb61b4e824ed9ec5c7e7
~noodlez1232 <contact@nathanielbarragan.xyz>
2024-04-24 20:40:51 -0700

    Update mainline

--- suyu-mainline.nix
+++ suyu-mainline.nix
@@ -5,8 +5,8 @@ let
     domain = "git.suyu.dev";
     owner = "suyu";
     repo = "suyu";
-    rev = "3f76206e9bba0fbb86f62c7a3494bd2f985e8524";
-    hash = "sha256-TFLKhkIT4vnRAzPDZDuO2F7ifmBGTS9eD+WxRCct8l8=";
+    rev = "dfb9f06e5c46f251e4208adf1d4861e85b1d5eea";
+    hash = "sha256-Jycpnfnx71alnxu+KgnOvaO26diEspx3O1dhA9OGflI=";
     fetchSubmodules = true;
   };
   
